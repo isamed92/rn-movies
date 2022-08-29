7b859ca52c2e3700872b37ffb46322cbf9982475
Isaias Medina <isamed92@gmail.com>
2022-08-29 01:13:59 -0300

    add navigation stack

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { Navigation } from './src/navigation/Navigation';
+
 
 const App = () => {
   return (
     <NavigationContainer>
-
-      
+      <Navigation/>
     </NavigationContainer>
   )
 }
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+// import { useNavigation } from '@react-navigation/native'
+
+export const HomeScreen = () => {
+
+  // const navigation = useNavigation();
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+
+      {/* <Button title='Ir a detalle' onPress={()=> navigation.navigate('DetailScreen') } /> */}
+    </SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({})
